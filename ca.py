1027bca30a515e08c9e939718646601984d3b08f
arnaudhe <arnaud.henric@gmail.com>
2020-03-28 23:06:24 +0100

    Use xlsx format

--- ca.py
+++ ca.py
@@ -1,47 +1,61 @@
-import csv
+import xlrd
 import sys
-import math
-import re
 
-if len(sys.argv) < 2:
-    print "usage : python soge.py filename.tsv"
-    sys.exit()
+COLUMN_DATE         = 0
+COLUMN_DESCRIPTION  = 1
+COLUMN_DEBIT        = 2
+COLUMN_CREDIT       = 3
 
-CA_SYNTAX = \
-[{'filter' : 'Virement En Votre Faveur', 'format' : 'VIR - {0}'},                 \
- {'filter' : 'Prelevmnt',                'format' : 'PRELEV - {0}'},              \
- {'filter' : 'Prelevement',              'format' : 'PRELEV - {0}'},              \
- {'filter' : 'Cotisation',               'format' : 'FRAIS BANCAIRES - {0}'}]
+CA_SYNTAX = [
+    {'filter' : 'VIREMENT EN VOTRE FAVEUR', 'format' : 'VIREMENT - {}', 'rtrim' : 1},
+    {'filter' : 'VIREMENT EMIS',            'format' : 'VIREMENT - {}', 'rtrim' : 5},
+    {'filter' : 'PAIEMENT PAR CARTE',       'format' : 'CARTE - {}', 'rtrim' : 5}, 
+    {'filter' : 'PRELEVEMENT',              'format' : 'PRELEVMT - {}', 'rtrim' : 1}, 
+    {'filter' : 'CHEQUE EMIS',              'format' : 'CHEQUE - {}', 'rtrim' : 1},
+    {'filter' : 'REGLEMENT',                'format' : 'REGLEMNT - {}', 'rtrim' : 5},
+    {'filter' : 'REMBOURSEMENT DE PRET',    'format' : 'RBT PRET - {}', 'rtrim' : 8},
+    {'filter' : 'RETRAIT AU DISTRIBUTEUR',  'format' : 'RETRAIT - {}', 'rtrim' : 12}
+]
 
-def format_motif(motif):
-    lines = motif.split(" \r\n")
+def sanitize_description(description):
+    lines = [line.strip() for line in description.split("\n")]
     for syntax in CA_SYNTAX:
-        if syntax['filter'] == lines[1].strip():
-            return syntax['format'].format(*lines).upper()
+        if syntax['filter'] == lines[0]:
+            return syntax['format'].format(lines[1][:-syntax['rtrim']].strip())[:43].upper()
     # no filter found
     return ' - '.join(lines).upper()
 
+def find_first_row(sheet):
+    row = 0
+    while sheet.cell_value(row, 0) != 'Date':
+        row = row + 1
+    return row + 1       # Skip table header
+
+# Load the xlsx sheet
+sheet = xlrd.open_workbook(sys.argv[1]).sheet_by_index(0)
+
+# Retrieve the first row index
+row = find_first_row(sheet)
+
+# Initilialize the lists
 debits  = []
 credits = []
 
-with open(sys.argv[1]) as tsv:
+while row < sheet.nrows:
+    record = [sheet.cell_value(row, COLUMN_DATE),
+             sanitize_description(sheet.cell_value(row, COLUMN_DESCRIPTION)),
+             str(sheet.cell_value(row, COLUMN_DEBIT)).replace('.', ',')]
+    if record[2] != '':
+        debits.append(record)
+    else:
+        record[2] = str(sheet.cell_value(row, COLUMN_CREDIT)).replace('.', ',')
+        credits.append(record)
 
-    # discard the 2 first lines
-    for i in range(11):
-        next(tsv)
+    row = row + 1
 
-    for line in csv.reader(tsv, delimiter=";", lineterminator=";\n"): #You can also use delimiter="\t" rather than giving a dialect.
-        if (len(line) >= 4):
-            date        = line[0].strip()
-            libelle     = format_motif(line[1])
-            debit       = line[2].strip()
-            credit      = line[3].strip()
+debits.reverse()
+credits.reverse()
 
-            if (len(debit) > 0):
-                debits.append("{}\t{}\t{}".format(date, libelle, debit))
-            else:
-                credits.append("{}\t{}\t{}".format(date, libelle, credit))
-
-    print "\n".join(debits)
-    print ' '
-    print "\n".join(credits)
+print('\n'.join(['\t'.join(record) for record in debits]))
+print('\n')
+print('\n'.join(['\t'.join(record) for record in credits]))
